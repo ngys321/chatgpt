c5460c228f382fdbc4fb1ddc9016589331fc7065
Youngsoo Namgoong <ngys321@gmail.com>
2023-09-22 01:32:15 +0900

    feat: organize dataset files

--- dataset/xlcost2cosqa.py
+++ dataset/xlcost2cosqa.py
@@ -19,14 +19,24 @@ if __name__ == "__main__":
     # output_file_path = "cosqa_da.json"
     # xlcost2cosqa(input_file_path, output_file_path)
 
-    input_file_path = "cosqa_da_spt_.json"
-    output_file_path = "cosqa_da_spt.json"
-    xlcost2cosqa(input_file_path, output_file_path)
+    # input_file_path = "cosqa_da_spt_.json"
+    # output_file_path = "cosqa_da_spt.json"
+    # xlcost2cosqa(input_file_path, output_file_path)
 
-    input_file_path = "cosqa_da_sro_.json"
-    output_file_path = "cosqa_da_sro.json"
-    xlcost2cosqa(input_file_path, output_file_path)
+    # input_file_path = "cosqa_da_sro_.json"
+    # output_file_path = "cosqa_da_sro.json"
+    # xlcost2cosqa(input_file_path, output_file_path)
+
+    # input_file_path = "cosqa_da_srm_.json"
+    # output_file_path = "cosqa_da_srm.json"
+    # xlcost2cosqa(input_file_path, output_file_path)
 
     input_file_path = "cosqa_da_spt_sro_.json"
     output_file_path = "cosqa_da_spt_sro.json"
+    xlcost2cosqa(input_file_path, output_file_path)
+    input_file_path = "cosqa_da_spt_srm_.json"
+    output_file_path = "cosqa_da_spt_srm.json"
+    xlcost2cosqa(input_file_path, output_file_path)
+    input_file_path = "cosqa_da_spt_sro_srm_.json"
+    output_file_path = "cosqa_da_spt_sro_srm.json"
     xlcost2cosqa(input_file_path, output_file_path)
